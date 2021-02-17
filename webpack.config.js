f286b1c3ae1c0614c50457387bfd76ce8c7da944
shunwitter <3123900+shunwitter@users.noreply.github.com>
2021-02-17 18:55:58 +0900

    Use html-loader with HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,15 @@ module.exports = {
           // },
         ],
       },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
