60b28e982967531963c4cfbe1b7e944828c69037
shunwitter <3123900+shunwitter@users.noreply.github.com>
2021-10-04 10:16:28 +0900

    Install lax.js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,14 @@
 import './reactApp.jsx';
 import '../stylesheets/main.scss';
+
+
+import lax from 'lax.js';
+window.onload = function () {
+  window.lax = { presets: lax.presets }
+  lax.init();
+  console.log('lax initialized');
+  // Add a driver that we use to control our animations
+  lax.addDriver('scrollY', function () {
+    return window.scrollY;
+  });
+}
